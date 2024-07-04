e7d469676834e6af820f5d825c16a4056c7030ea
hslee <vawav11@gmail.com>
2024-07-04 22:47:14 +0900

    fix: tw config 설정

--- next15-social/tailwind.config.ts
+++ next15-social/tailwind.config.ts
@@ -5,6 +5,14 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/b.pages/**/*.{ts,tsx}',
+    './src/c.widgets/**/*.{ts,tsx}',
+    './src/d.features/**/*.{ts,tsx}',
+    './src/e.entities/**/*.{ts,tsx}',
+    './src/f.shared/**/*.{ts,tsx}',
+    // './pages/**/*.{ts,tsx}',
+    // './components/**/*.{ts,tsx}',
+    // './app/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
