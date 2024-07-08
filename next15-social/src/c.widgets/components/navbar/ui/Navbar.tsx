021b0193414440a655d31588368f59409c51dc39
hslee <vawav11@gmail.com>
2024-07-08 21:30:19 +0900

    fix: 폴더,파일 위치 변경

--- next15-social/src/c.widgets/components/navbar/ui/Navbar.tsx
+++ next15-social/src/c.widgets/components/navbar/ui/Navbar.tsx
@@ -1,8 +1,8 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { MobileMenu } from "@/f.shared/components/mobile-menu";
 import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { MobileMenu } from "../../mobile-menu";
 
 export function Navbar() {
   return (
