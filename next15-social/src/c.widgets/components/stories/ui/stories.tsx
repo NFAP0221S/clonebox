0d319a2a6450bc5b97e560416d4b8cedf4ed60b9
hslee <vawav11@gmail.com>
2024-07-09 22:00:40 +0900

    Update stories.tsx

--- next15-social/src/c.widgets/components/stories/ui/stories.tsx
+++ next15-social/src/c.widgets/components/stories/ui/stories.tsx
@@ -6,7 +6,11 @@ export function Stories() {
       <div className="flex gap-8 w-max">
         {/* <StoryList stories={stories} userId={currentUserId}/> */}
         <div className='flex flex-col items-center gap-2 cursor-pointer'>
-          {/* <Image src="https://imgaes.pexels.com/photos/13172901/pexels-photo-13172901.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" width={15} height={15}  /> */}
+          <Image src="https://images.pexels.com/photos/17351400/pexels-photo-17351400.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image src="https://images.pexels.com/photos/17351400/pexels-photo-17351400.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
           <span className="font-medium">Ricky</span>
         </div>
       </div>
