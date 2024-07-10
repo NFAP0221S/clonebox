0714716bc7e39f49d74902d73055389f295ea02f
hslee <vawav11@gmail.com>
2024-07-10 23:13:25 +0900

    style: feed style, 더미 피드

--- next15-social/src/c.widgets/components/feed/ui/feed.tsx
+++ next15-social/src/c.widgets/components/feed/ui/feed.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Post } from "@/c.widgets/components/post";
 
 export function Feed() {
   return (
-    <div>Feed</div>
+    <div className='p-4 bg-white shadow-md rounded-lg flex flex-col gap-12'>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
   )
 }
