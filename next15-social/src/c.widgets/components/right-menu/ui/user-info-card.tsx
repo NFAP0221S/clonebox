f2fce282b559182df17a2d79a09fb7d2be6a7105
hslee <vawav11@gmail.com>
2024-07-16 21:27:27 +0900

    style: UserInfoCard, UserMediaCard

--- next15-social/src/c.widgets/components/right-menu/ui/user-info-card.tsx
+++ next15-social/src/c.widgets/components/right-menu/ui/user-info-card.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React from 'react'
+import Image from 'next/image'
 
 interface Props {
   userId?: string
@@ -12,6 +12,48 @@ export function UserInfoCard({userId}: Props) {
       <div className='flex justify-between items-center font-medium'>
         <span className='text-gray-500'>User Information</span>
         <Link href='/' className='text-blue-500 text-xs'>See all</Link>
+      </div>
+      {/* BOTTOM */}
+      <div className='flex flex-col gap-4 text-gray-500'>
+        <div className='flex items-center gap-2'>
+          <span className='text-xl text-black'>정xx</span>
+          <span className='text-sm'>@조xx</span>
+        </div>
+        <p>
+          sdfsdfsdfsdfsdfdsfsdfsdf.
+        </p>
+        <div className='flex items-center gap-2'>
+          <Image src="/map.png" alt='' width={16} height={16}/>
+          <span>
+            Living in <b>Denver</b>
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Image src="/school.png" alt='' width={16} height={16}/>
+          <span>
+            Went to <b>Edgar High School</b>
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Image src="/work.png" alt='' width={16} height={16}/>
+          <span>
+            Works at <b>Apple Inc.</b>
+          </span>
+        </div>
+        <div className='flex items-center justify-between'>
+          <div className='flex gap-1 items-center'>
+            <Image src="/link.png" alt='' width={16} height={16}/>
+            <Link href='https://lama.dev' className='text-blue-500 font-medium'>
+              lama.dev
+            </Link>
+          </div>
+          <div className='flex gap-1 items-center'>
+            <Image src="/date.png" alt='' width={16} height={16}/>
+            <span>Joined November 2024</span>
+          </div>
+        </div>
+        <button className='bg-blue-500 text-white text-sm rounded-md p-2'>Follow</button>
+        <span className='text-red-400 self-end text-xs cursor-pointer'>Block User</span>
       </div>
     </div>
   )
--- next15-social/src/c.widgets/components/right-menu/ui/user-media-card.tsx
+++ next15-social/src/c.widgets/components/right-menu/ui/user-media-card.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import React from 'react'
+import Image from 'next/image'
+import { $image } from '@/f.shared/dummy'
 
 interface Props {
   userId?: string
@@ -11,7 +12,76 @@ export function UserMediaCard({userId}: Props) {
       {/* TOP */}
       <div className='flex justify-between items-center font-medium'>
         <span className='text-gray-500'>User Media</span>
-        <Link href='/' className='text-blue-500 text-xs'>See all</Link>
+        <Link href='/' className='text-blue-500 text-xs'>
+          See all
+        </Link>
+      </div>
+      {/* BOTTOM */}
+      <div className='flex gap-4 justify-between flex-wrap'>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src={$image} 
+            alt='' 
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
       </div>
     </div>
   )
