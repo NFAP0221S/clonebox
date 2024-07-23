78474aa0a74c5934da8251b2838221e68ecde9cf
hslee <vawav11@gmail.com>
2024-07-23 21:30:22 +0900

    test: db, server action 테스트

--- next15-social/src/c.widgets/components/add-post/ui/add-post.tsx
+++ next15-social/src/c.widgets/components/add-post/ui/add-post.tsx
@@ -1,7 +1,33 @@
 import { $image } from '@/f.shared/dummy'
+import prisma from '@/f.shared/lib/client';
+import { auth } from '@clerk/nextjs/server';
 import Image from 'next/image'
 
 export function AddPost() {
+
+  const {userId} = auth()
+  
+  console.log(userId)
+  
+  const testAction = async (formData: FormData) => {
+    "use server"
+
+    if (!userId) return;
+
+    const desc = formData.get("desc") as string;
+    try {
+      const res = await prisma.post.create({
+        data: {
+          userId: userId,
+          desc: desc,
+        }
+      })
+      console.log('res 확인:', res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className='p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm' title='포스터 추가'>
       {/* AVATAR */}
@@ -15,8 +41,12 @@ export function AddPost() {
       {/* POST */}
       <div className='flex-1'>
         {/* TEXT INPUT */}
-        <div className='flex gap-4'>
-          <textarea placeholder='어떤 생각을 하시나요?' className='flex-1 bg-slate-100 rounded-lg p-2'></textarea>
+        <form action={testAction} className='flex gap-4'>
+          <textarea 
+            placeholder='어떤 생각을 하시나요?'
+            className='flex-1 bg-slate-100 rounded-lg p-2'
+            name='desc'
+          />
           <Image
             src="/emoji.png"
             alt=""
@@ -24,7 +54,8 @@ export function AddPost() {
             height={20}
             className="w-5 h-5 cursor-pointer self-end"
           />
-        </div>
+          <button>Send</button>
+        </form>
         {/* POST OPTIONS */}
         <div className='flex items-center gap-4 mt-4 text-gray-400 flex-wrap'>
           <div className='flex items-center gap-2 cursor-pointer'>
