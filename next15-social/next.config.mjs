bee324d0107e46956c44afb9f76208c60b752c1d
hslee <vawav11@gmail.com>
2024-07-26 20:21:43 +0900

    feat: left menu 프로필 noCover 적용

--- next15-social/next.config.mjs
+++ next15-social/next.config.mjs
@@ -10,7 +10,15 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
-    ]
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   }
 };
 
